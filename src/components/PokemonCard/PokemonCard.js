4673953fd7b23bf23a551f582d8234d7d95eeeff
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2023-10-28 23:43:46 +0100

    Pokemon card clean up

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -9,7 +9,7 @@ import Moves from "./Moves";
 import Stats from "./Stats";
 import Types from "./Types";
 
-const PokemonCard = (props) => {
+const PokemonCard = () => {
 
     const { id } = useParams();
     const [getPokemon, setPokemon] = useState();
@@ -17,7 +17,6 @@ const PokemonCard = (props) => {
     let currentPokemon;
     let monSprites = [];
     let artwork = "";
-    let artworkShiny = "";
     let abilitiesList = [];
     let monMoves = [];
     let monStats = [];
@@ -39,9 +38,7 @@ const PokemonCard = (props) => {
       if(hasResultReturned) {
         currentPokemon = getPokemon;
         monSprites = currentPokemon["sprites"];
-    
-        artwork = currentPokemon["sprites"]["other"]["official-artwork"]
-        artworkShiny = currentPokemon["sprites"]["other"]["official-artwork"]["front_shiny"]
+        artwork = currentPokemon["sprites"]["other"]["official-artwork"];
         abilitiesList = currentPokemon["abilities"];
         monMoves = currentPokemon["moves"];
         monStats = currentPokemon["stats"];
@@ -53,7 +50,6 @@ const PokemonCard = (props) => {
         <div>
             <div className={classes["hero-section"]}>
                 <div className={classes["artwork-section"]}>
-                    
                     <HeroImage images={artwork} />
                 </div>
                 <div className={classes["text-section"]}>
@@ -68,7 +64,6 @@ const PokemonCard = (props) => {
         
             <Sprites sprites={monSprites} />
             <Moves moves={monMoves} />
-            <img src={artworkShiny} alt=""/>
         </div>
     )
 }
